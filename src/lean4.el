2fae361e5d721ae285b01801f10f2c78ccdba9b7
t4ccer <t4ccer@gmail.com>
2023-09-10 20:37:34 -0600

    Add lean4 setup

---
+++ src/lean4.el
@@ -0,0 +1,10 @@
+(use-package lean4-mode
+  :straight (lean4-mode
+	     :type git
+	     :host github
+	     :repo "leanprover/lean4-mode"
+	     :files ("*.el" "data"))
+  ;; to defer loading the package until required
+  :commands (lean4-mode)
+  :config
+  (add-hook 'lean4-mode-hook #'lsp-deferred))
