6051b1bb23a15e52fa76ab8d835602b9d48d9243
t4ccer <t4ccer@gmail.com>
2024-02-01 21:01:59 -0700

    Company mode prefix len fix for LSP mode lag

--- src/company.el
+++ src/company.el
@@ -8,7 +8,7 @@
   (haskell-mode . company-mode)
   (emacs-lisp-mode . company-mode)
   :config
-  (setq company-minimum-prefix-length 0)
+  (setq company-minimum-prefix-length 1)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
   ;; (define-key company-active-map (kbd "<return>") 'company-complete-selection)
