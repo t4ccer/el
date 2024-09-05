7fef7aebc84d7af04bff684d70eb5a32f3a7dd1f
t4ccer <t4ccer@gmail.com>
2024-09-04 18:27:12 -0600

    Fix rust with envrc

--- init.el
+++ init.el
@@ -202,7 +202,7 @@
 (use-package fasm-mode
   :ensure t
   :straight (:host github :repo "the-little-language-designer/fasm-mode"))
-(add-to-list 'auto-mode-alist '("\\.asm\\'" . fasm-mode))
+(add-to-list 'auto-mode-alist '("\\.asm\\'" . asm-mode))
 
 (use-package string-inflection
   :ensure t)
--- src/envrc.el
+++ src/envrc.el
@@ -8,5 +8,6 @@
   (envrc-global-mode)
   (add-hook 'agenix-pre-mode-hook #'envrc-mode)
   (add-hook 'haskell-mode-hook #'envrc-mode)
+  (add-hook 'rustic-mode-hook #'envrc-mode)
   ;; Fix for not applying envrc correctly
   (add-hook 'eshell-directory-change-hook (lambda () (progn (envrc-mode) (envrc-mode)))))
