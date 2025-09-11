8920fd371943e376ca0b54b780135516deddd4a8
t4ccer <t4ccer@gmail.com>
2025-09-11 11:29:48 -0230

    Fix rename action in HLS

--- src/haskell.el
+++ src/haskell.el
@@ -16,4 +16,8 @@
   (setq lsp-haskell-plugin-import-lens-code-lens-on nil)
   (setq lsp-haskell-plugin-import-lens-code-actions-on nil)
   (setq lsp-haskell-plugin-ghcide-type-lenses-config-mode nil)
-  (setq lsp-haskell-plugin-stan-global-on nil))
+  (setq lsp-haskell-plugin-stan-global-on nil)
+  :custom
+  ; https://github.com/emacs-lsp/lsp-haskell/issues/192
+  (lsp-rename-use-prepare nil)
+  )
