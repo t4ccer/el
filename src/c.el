fa814f88cc5e3f1c6952a28ebdbff3510af84849
t4ccer <t4ccer@gmail.com>
2026-03-14 21:02:57 -0230

    Fix indentation and tabs in C

--- src/c.el
+++ src/c.el
@@ -7,7 +7,12 @@
   (add-hook 'c++-mode-hook #'lsp-deferred)
   (add-hook 'c++-mode-hook
             (lambda ()
-              (local-set-key  (kbd "C-c o") 'ff-find-other-file))))
+              (local-set-key  (kbd "C-c o") 'ff-find-other-file)))
+  (add-hook 'c-mode-common-hook
+          (lambda ()
+            (setq c-basic-offset 4)
+            (setq indent-tabs-mode nil)))
+)
 
 
 
